a9b1091a7968a661abff7da05909e96f5a63e7d5
Sarun Dhoju <sarundhoju775@gmail.com>
2026-01-05 15:26:50 +0545

    giles added

---
+++ usermgmt/dashboard.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>dashboard</title>
+</head>
+<body>
+    <h1>hello welcome to my profile</h1>
+    <button class = "btn">Logout</button>
+</body>
+</html>
--- usermgmt/login.php
+++ usermgmt/login.php
@@ -1,34 +1,84 @@
+<?php
+// Start the session
+session_start();
+include'./connection.php';
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $remember = isset($_POST['remember']) ? true : false;
+    
+    //basic validation
+    if(empty($username) || empty($password)){
+        $_SESSION['error'] = 'Please fill in all fields.';
+    }else{
+        //prepare and execute the query
+        $sql="SELECT id, username, password FROM tbl_users WHERE username = '$username' OR email = '$username' LIMIT 1";
+        $res = mysqli_query($conn, $sql);
+        if($res->num_rows == 1){
+            echo 'test';
+            $user = $res->fetch_assoc();
+            //verify password
+            $haspwd = sha1($password);
+            if($haspwd == $user['password']){
+                $_SESSION['user_id'] = $user['id'];
+                if($remember){
+                    setcookie('user_login', $user['username'], time() + (86400 * 30), "/");  //30 days
+                   }
+                   header("location: dashboard.php");
+            }else{
+            $_SESSION['error'] = 'Invalid password.';
+            }
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | User Management</title>
+    <title>Login</title>
     <link rel="stylesheet" href="./main.css">
 </head>
 <body>
     <main class="container">
-        <h1 class="page-title">Login</h1>
-        <form action="#" method="post" name="user_form">
-            <div class="field-group">
-                <label for="uname">Username / E-mail</label>
-                <input type="text" id="uname" name="username">
+        <h1 class="page-title"> Login </H1>
+        <?php if (isset($_SESSION['message'])): ?>
+            <div class="alert success">
+                <?php 
+                    echo $_SESSION['message'];// display success message 
+                    unset($_SESSION['message']); // remove message after displaying ?>
+                    <?php elseif (isset($_SESSION['error'])): ?>
+            <div class="alert error">
+                <?php 
+                    echo $_SESSION['error']; // display error message
+                    unset($_SESSION['error']);// remove message after displaying?>
             </div>
-            <div class="field-group">
-                <label for="pwd">Password</label>
-                <input type="password" id="pwd" name="password">
-            </div>
-            <div class="field-group">
-                <input type="checkbox" id="rem" name="remember">
-                <label for="rem">Remember Login credentials.</label>
-            </div>
-            <button type="submit" name="submit" class="btn">Login</button>
-        </form>
-         <div class="btn-group">
-                <span class="note">don't have an account? <a href = "register.php" class="text-link">Register Now </a></span> <br><br>
-                <a href = "index.php" class="text-link" title="Back to Home">&larr; Back to Home </a>
+        <?php endif; ?>
 
-    </div>
-    </main>
+
+        <form action="#" method="post" name="user_form">
+            <div class="form-group">
+            <label for="uname">Username / Email</label>
+            <input type = "text" id="uname" name="username" value="">
+        </div>
+        <div class="form-group">
+            <label for="PWD">password</lebel>
+            <input type = "password" id="PWD" name="password" value="">
+        </div>
+        <div class="field-group">
+            <input type ="checkbox" id="rem" name ="remember" value="1">
+            <label for="rem">Remember login credentials</label>
+        </div>
+        <button type="submit" name="submit" class="btn">login</button>
+         <div class="btn_group">
+            <span class="note">Don't have an account?  <a href="register.php" class="Text link">Register Now</a></span>
+   <br>
+    <a href ="index.php" class="Text link">&larr; Back to Home</a>
+</div>
+        </form>
+</main>
 </body>
 </html>
